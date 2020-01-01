75d5b1d1c744a7521fe688915403412113979344
cnxsliu <1283216796@qq.com>
2020-01-01 21:19:54 +0800

    add strategy

--- src/main/java/cn/edu/zju/aimforoffer/algorithm/A002Singleton.java
+++ src/main/java/cn/edu/zju/aimforoffer/algorithm/A002Singleton.java
@@ -4,7 +4,11 @@ package cn.edu.zju.aimforoffer.algorithm;
  * @description: 单例设计模式，线程懒汉式：静态内部类
  * @author: cnxsliu
  * @date: 2019/12/9 20:41
+ * @strategy:
+ * 1、
+ *
  */
+
 public class A002Singleton {
 
     private static class SingletonHolder{
@@ -14,6 +18,7 @@ public class A002Singleton {
     public static A002Singleton getInstance(){
         return SingletonHolder.ourInstance;
     }
+
     private A002Singleton(){
     }
 

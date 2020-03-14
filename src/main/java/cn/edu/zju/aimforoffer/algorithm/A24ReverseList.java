1749ac2a72efe297c4e5ce30b50255de3fbeccb4
cnxsliu <1283216796@qq.com>
2020-03-14 17:33:46 +0800

    add the result of question024

---
+++ src/main/java/cn/edu/zju/aimforoffer/algorithm/A24ReverseList.java
@@ -0,0 +1,27 @@
+package cn.edu.zju.aimforoffer.algorithm;
+
+/**
+ * @author cnxsliu
+ * @description
+ * @date 2020/3/14 17:27
+ * @strategy
+ * 1、需要顶一个三个指针（节点），分别用来存储当前节点的下一个节点，当前节点，
+ * 当前节点的前一个节点。
+ * 2、将当前节点指向前一个节点。
+ * 3、将当前节点赋值给前一个节点。
+ * 4、将后一个节点赋值给当前节点。
+ */
+public class A24ReverseList {
+    public ListNode reverseList (ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        ListNode after = null;
+        while (curr != null) {
+             after = curr.next;
+             curr.next = pre;
+             pre = curr;
+             curr = after;
+        }
+        return pre;
+    }
+}

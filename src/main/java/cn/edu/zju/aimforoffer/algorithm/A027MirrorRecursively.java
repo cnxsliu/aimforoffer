4a937117307edbc7b2381c89e6fbbc8607bfdce9
cnxsliu <1283216796@qq.com>
2020-03-22 20:21:38 +0800

    add the result of question027

---
+++ src/main/java/cn/edu/zju/aimforoffer/algorithm/A027MirrorRecursively.java
@@ -0,0 +1,26 @@
+package cn.edu.zju.aimforoffer.algorithm;
+
+/**
+ * @author cnxsliu
+ * @description
+ * @date 2020/3/22 20:16
+ * @strategy
+ * 1、使用递归方法，对每个节点的左右子节点进行遍历
+ * 2、将每个节点的左节点和右节点进行交换
+ * 3、终止条件为：1.根节点为空；2.根节点的左右节点都为空
+ */
+public class A027MirrorRecursively {
+    public void Mirror (TreeNode root) {
+        if (root == null) {
+            return ;
+        }
+        if (root.left == null && root.right == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.left = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}

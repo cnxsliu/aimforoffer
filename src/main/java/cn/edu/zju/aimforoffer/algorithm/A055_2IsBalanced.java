77df6ac1066f61b66b88476714f93003dde8bd9d
cnxsliu <1283216796@qq.com>
2020-04-12 15:58:56 +0800

    add code of quesiton 55-2

--- src/main/java/cn/edu/zju/aimforoffer/algorithm/A055_2IsBalanced.java
+++ src/main/java/cn/edu/zju/aimforoffer/algorithm/A055_2IsBalanced.java
@@ -5,6 +5,25 @@ package cn.edu.zju.aimforoffer.algorithm;
  * @description
  * @date 2020/4/12 15:40
  * @strategy
+ * 1、采用后序遍历，从低向上遍历判断，一旦出现不符合条件的情况，则直接返回
  */
-public class A052_2IsBalanced {
+public class A055_2IsBalanced {
+    public static boolean isBalanced(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+    public static int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getDepth(root.left);
+        if (left == -1) {
+            return -1;
+        }
+        int right = getDepth(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+    }
+
 }

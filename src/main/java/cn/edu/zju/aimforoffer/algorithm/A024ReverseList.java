3db3ee4f88b9574f0b36422f81b75f03e55f242d
cnxsliu <1283216796@qq.com>
2020-05-26 23:15:03 +0800

    add some sort methods

--- src/main/java/cn/edu/zju/aimforoffer/algorithm/A024ReverseList.java
+++ src/main/java/cn/edu/zju/aimforoffer/algorithm/A024ReverseList.java
@@ -4,7 +4,11 @@ package cn.edu.zju.aimforoffer.algorithm;
  * @author cnxsliu
  * @description
  * @date 2020/3/14 17:27
- * @strategy 1、需要顶一个三个指针（节点），分别用来存储当前节点的下一个节点，当前节点，
+ * @strategy
+ * >> 共 12 行
+ * >> 注意：return pre
+ *
+ * 1、需要顶一个三个指针（节点），分别用来存储当前节点的下一个节点，当前节点，
  * 当前节点的前一个节点。
  * 2、将当前节点指向前一个节点。
  * 3、将当前节点赋值给前一个节点。
@@ -13,17 +17,13 @@ package cn.edu.zju.aimforoffer.algorithm;
 public class A024ReverseList {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode curr = head;
-        ListNode after = null;
-        while (curr != null) {
-            //将当前节点的下一个节点保存起来
-            after = curr.next;
-            //将当前节点指向前一个节点
-            curr.next = pre;
-            //将当前节点赋值给前一个节点
-            pre = curr;
-            //将后一个节点赋值给当前节点
-            curr = after;
+        ListNode cur = head;
+        ListNode after;
+        while (cur != null) {
+            after = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = after;
         }
         return pre;
     }
